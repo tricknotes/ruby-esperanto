9dfa2e4d7ff855e1fdea6efbf9e86a889ec06f98
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-02-07 21:56:11 +0900

    Support esperanto transpilers other than AMD

--- lib/esperanto.rb
+++ lib/esperanto.rb
@@ -9,9 +9,18 @@ module Esperanto
       @context ||= ExecJS::Runtimes::Node.compile(Source.bundled_paths.map {|path| File.read(path) }.join(';'))
     end
 
-    def transform(code, options = {})
-      # TODO Support other format
+    def toAmd(code, options = {})
       context.call('esperanto.toAmd', code, options)
     end
+
+    def toCjs(code, options = {})
+      context.call('esperanto.toCjs', code, options)
+    end
+
+    def toUmd(code, options = {})
+      context.call('esperanto.toUmd', code, options)
+    end
+
+    # TODO Support `esperanto.bundle`. see: https://github.com/esperantojs/esperanto/wiki/Bindings
   end
 end
